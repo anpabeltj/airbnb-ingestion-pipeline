ea91e59b6d500a195c5c23222459acc3107d0239
anpabeltj <patrapembinaan@gmail.com>
2026-04-29 20:54:59 +0800

    feat: add dag script to scheduling transformation from csv to postgresql database and metabase

---
+++ dags/airbnb-dag.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+import sys
+sys.path.insert(0, '/opt/airflow/scripts')
+import ingest as i
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+default_args = {
+    'owner' : 'airflow',
+    'start_date' : datetime(2024, 1, 1),
+    'retries' : 1
+}
+
+with open ('/opt/airflow/scripts/transform.sql', 'r') as f:
+    sql_query = f.read()
+
+with DAG('airbnb_ingestion_pipeline', default_args=default_args, schedule='@daily', catchup=False) as dag:
+    ingest_task = PythonOperator(
+        task_id = 'ingest_airbnb',
+        python_callable = i.ingest_all
+    )
+    transform_task = PostgresOperator(
+        task_id = 'transform_airbnb',
+        postgres_conn_id = 'postgres_airbnb',
+        sql = sql_query
+    )
+    ingest_task >> transform_task
